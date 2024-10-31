9825d42f40dfa9e32afa2f7f916faca0e3c24a9e
SeshuTarapatla <seshu.tarapatla@gmail.com>
2024-10-31 23:02:47 +0530

    Code

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Fetch-Spotlight
-A python project that can fetch your current Windows Spotlight wallpaper into your library
+A python project that can fetch your current Windows Spotlight wallpaper into your library.
+
+## What it does
+Creates a new folder "Windows Spotlight" in your Pictures library & copied your current Spotlight wallpaper into that folder.
+
+File naming format : **"Windows-Spotlight-{Current_date}_{index}.jpg"**  
+Index: Starts at 01 (and gets incremented if there is different wallpaper at same date).
+  
+## Build for windows
+It is converted into Windows Executable using Python 3.12 and a library called Nuitka.
+
+## Refs
+1. Nuitka: https://github.com/Nuitka/Nuitka
---
+++ windows-spotlight.py
@@ -0,0 +1,75 @@
+from hashlib import md5
+from os import environ, path, makedirs, listdir
+from datetime import datetime
+from shutil import copy2
+from PIL import Image
+from rich import print
+
+
+APPDATA = environ.get('appdata')
+USERPROFILE = environ.get('userprofile')
+SOURCE_FILE = path.join(APPDATA,'Microsoft','Windows','Themes','TranscodedWallpaper')
+DESTINATION_DIRECTORY = path.join(USERPROFILE,'Pictures','Windows Spotlight')
+
+
+INFO = lambda log: print(f'[[bold blue]INFO[/bold blue]] {log}')
+ERROR = lambda log: print(f'[[bold red]ERROR[/bold red]] {log}')
+
+
+class Spotlite:
+    def __init__(self):
+        makedirs(DESTINATION_DIRECTORY, exist_ok=True)
+
+        self.date = datetime.today().strftime('%Y-%m-%d')
+        self.index = 1
+        self.prefix = 'Windows-Spotlight'
+        self.last_wallpaper = self.fetch_last_wallpaper()
+
+    def fetch_last_wallpaper(self):
+        last_wallpaper = sorted(
+            filter(
+                lambda x: x.startswith(self.prefix),
+                listdir(DESTINATION_DIRECTORY)
+            ),
+            reverse=True
+        )[0]
+        return path.join(DESTINATION_DIRECTORY, last_wallpaper)
+
+    def fetch_current_wallpaper(self):
+        try:
+            self.check_duplicate()
+            output = self.generate_output_filename()
+            copy2(SOURCE_FILE, output)
+            INFO(f'Wallpaper copied: "[blue]{path.basename(output)}[/blue]"')
+        except FileExistsError:
+            INFO(f'Wallpaper already copied.')
+
+    def check_duplicate(self):
+        hash1 = generate_hash(SOURCE_FILE)
+        hash2 = generate_hash(self.last_wallpaper)
+
+        if hash1 == hash2:
+            INFO(f'Hashes match (hash1: [italic]{hash1}[/italic], hash2: [italic]{hash2})[/italic]')
+            raise FileExistsError
+        else:
+            INFO(f'Hashes doesn\'t match (hash1: [italic]{hash1}[/italic], hash2: [italic]{hash2})[/italic]')
+        return None
+
+    def generate_output_filename(self):
+        while True:
+            file = path.join(DESTINATION_DIRECTORY, f'{self.prefix}-{self.date}_{str(self.index).rjust(2,"0")}.jpg')
+            if path.exists(file): self.index += 1
+            else: return file
+
+def generate_hash(file: str):
+    try:
+        INFO(f'Generating Hash for "[blue]{path.basename(file)}[/blue]"')
+        with Image.open(file) as img:
+            return md5(img.tobytes()).hexdigest()
+    except FileNotFoundError:
+        ERROR(f'FileNotFound: [blue]"{file}"[/blue]')
+        return None
+
+
+if __name__ == '__main__':
+    Spotlite().fetch_current_wallpaper()
