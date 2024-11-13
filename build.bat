b39f01b3d2323ef38181f5255ab3df5b04280f96
SeshuTarapatla <seshu.tarapatla@gmail.com>
2024-11-13 23:00:46 +0530

    Notifications added

--- build.bat
+++ build.bat
@@ -2,8 +2,9 @@
 
 echo Build Windows executable using Nuitka
 python -m nuitka --onefile ".\windows-spotlight.py" ^
-    --output-filename=Fetch-Spotlight ^
-    --windows-icon-from-ico=".\icons\spotlight.ico"
+    --output-filename="Fetch Spotlight" ^
+    --windows-icon-from-ico=".\icons\spotlight.ico" ^
+    --include-data-files=".\icons\spotlight.ico"="icons\spotlight.ico"
 
 echo Nuitka: Deleting cache directories.
 rmdir /S /Q ".\windows-spotlight.build"
--- windows-spotlight.py
+++ windows-spotlight.py
@@ -1,22 +1,28 @@
+from datetime import datetime
 from hashlib import md5
 from os import environ, path, makedirs, listdir
-from datetime import datetime
 from shutil import copy2
+from subprocess import Popen, CREATE_NO_WINDOW
+from sys import argv, executable
+
 from PIL import Image
+from plyer import notification
 from rich import print
 
-
+ARGS = argv + ['']
+EXECUTION_TYPE = path.splitext(ARGS[0])[-1]
 APPDATA = environ.get('appdata')
 USERPROFILE = environ.get('userprofile')
 SOURCE_FILE = path.join(APPDATA,'Microsoft','Windows','Themes','TranscodedWallpaper')
 DESTINATION_DIRECTORY = path.join(USERPROFILE,'Pictures','Windows Spotlight')
 
 
+DEBUG = lambda log: print(f'[[bold green]DEBUG[/bold green]] {log}')
 INFO = lambda log: print(f'[[bold blue]INFO[/bold blue]] {log}')
 ERROR = lambda log: print(f'[[bold red]ERROR[/bold red]] {log}')
 
 
-class Spotlite:
+class Spotlight:
     def __init__(self):
         makedirs(DESTINATION_DIRECTORY, exist_ok=True)
 
@@ -44,9 +50,13 @@ class Spotlite:
                 self.check_duplicate()
             output = self.generate_output_filename()
             copy2(SOURCE_FILE, output)
-            INFO(f'Wallpaper copied: "[blue]{path.basename(output)}[/blue]"')
+            message = 'Wallpaper copied'
+            INFO(f'{message}: "[blue]{path.basename(output)}[/blue]"')
+            show_notification(message)
         except FileExistsError:
-            INFO(f'Wallpaper already copied.')
+            message = 'Wallpaper already copied'
+            INFO(f'{message}')
+            if ARGS[1] != '-n': show_notification(message)
 
     def check_duplicate(self):
         hash1 = generate_hash(SOURCE_FILE)
@@ -74,6 +84,27 @@ def generate_hash(file: str):
         ERROR(f'FileNotFound: [blue]"{file}"[/blue]')
         return None
 
+def notify(message):
+    notification.notify(
+        title="Fetch Spotlight",
+        message=message,
+        app_icon="icons/spotlight.ico"
+    )
+
+def show_notification(message):
+    args = []
+    if EXECUTION_TYPE == '.exe':
+        args = [ARGS[0], '--show-notification', message]
+    elif EXECUTION_TYPE == '.py':
+        args = ['cmd', '/C', executable, ARGS[0], '--show-notification', message]
+    Popen(args, shell=True, creationflags=CREATE_NO_WINDOW)
+
+
+def main():
+    if ARGS[1] == '--show-notification':
+        notify(ARGS[2])
+    else:
+        Spotlight().fetch_current_wallpaper()
 
 if __name__ == '__main__':
-    Spotlite().fetch_current_wallpaper()
+    main()
